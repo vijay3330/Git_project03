404b7669486b89ee1f88b9ca3288e888f819cca6
AEPAC CLIENT <AEPAC CLIENT@DESKTOP-8H2JL49>
2022-12-26 20:20:05 +0530

    first commit

---
+++ src/Demo/Login2.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class Login2 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Yes i gave the credentials according to your suggetion");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
